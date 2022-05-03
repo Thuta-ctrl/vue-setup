966e0ff9d56caace680bd71e3662551028b786e5
zinnu0320 <62414481+zinnu0320@users.noreply.github.com>
2022-05-03 19:33:14 +0630

    modal update
    modal update

--- src/pages/modal.vue
+++ src/pages/modal.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <div class="modal-box" :class="{ show: showModal }">
-      <p>hello</p>
+      <div class="modal_content">
+        <p class="modal_ttl">Title</p>
+        <p class="modal_desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+</div>
     </div>
     <button @click="show">{{ showModal ? 'Hide':'Show' }}</button>
   </div>
@@ -26,12 +31,28 @@ export default {
 
 <style>
   .modal-box {
-    background: white;
-    width: 100px;
-    border: 1px solid red;
+    background: rgb(0, 0, 0, 0.7);
+    width: 100%;
+    height: 100vh;
     display: none;
+    position: relative;
   }
   .modal-box.show {
     display: block;
+  }
+  .modal_content{
+    width: 40%;
+    margin: 0 auto;
+    padding: 50px 100px;
+    background: #fff;
+    border-radius: 20px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    text-align: center;
+  }
+  .modal_ttl{
+    font-size: 22px;
   }
 </style>
