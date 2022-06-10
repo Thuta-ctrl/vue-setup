2e422a37cf071bae0702c9449362d17d0d09be3e
Thuta Yar Moe <thutayarmoe@Thutas-MacBook-Pro.local>
2022-06-10 10:16:17 +0630

    update calendar

--- src/pages/calendar.vue
+++ src/pages/calendar.vue
@@ -6,16 +6,23 @@
       title-position="left"
       :masks="masks"
       ref="datepicker"
-      :select-attribute="selectAttribute"
-      @dayclick="onDayClick"
-      :popover="{ visibility: 'click' }"
+      :value="date"
+      :popover="popover"
     >  
-      <template #day-content="{day}">
-        <div class="today" style="color: #FC346A" v-if="day.label == 16">{{ day.label }}</div>
-        <div class="others" @click="clickdate(day.id)" v-else>{{ day.label }}</div>
+      <template #day-content="{ day }" :ref="el => { lists[day.day] = el }">
+        <span 
+          @click="clickdate(day.id)" :aria-label="day.ariaLabel" 
+          aria-disabled="false" role="button" 
+          class="vc-day-content"
+          style="margin-left: 3px; margin-top: 2px"
+          :style="getStyle(day.id)"
+        >
+        {{ day.day }}</span>
       </template>
       <template #header-title="{ monthLabel, yearLabel }">
-        {{ yearLabel + '年' + monthLabel }}
+        <div>
+          {{ yearLabel + '年' + monthLabel }}
+        </div>
       </template>
       <template v-slot="{ inputValue, inputEvents }">
         <input
@@ -36,60 +43,53 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import 'v-calendar/dist/style.css'
 import { Calendar, DatePicker } from 'v-calendar';
 const date = ref(new Date())
-const year = date.value.getFullYear();
-const month = date.value.getMonth();
-const datepicker = ref(null)
-const selectAttribute = ref({
-  highlight: {
-    style: {
-      background: '#FC346A'
-    },
-    contentStyle: {
-      color: 'white'
-    }
-  }
-})
-const attributes = ref([
-  {
-    key: 'today',
-    highlight: {
-      contentStyle: {
-        color: '#FC346A'
-      }
-    },
-    dates: new Date(),
-  },
-  {
-    highlight: {
-      style: {
-        background: '#FC346A'
-      },
-      contentStyle: {
-        color: 'white'
-      }
-    },
-    dates: new Date(year, month, 9),
-  },
-])
 const masks = ref({
   input: 'YYYY-MM-DD',
 })
+const lists = ref([])
+const today = ref(new Date())
+const datepicker = ref(null)
+const popover = ref({ visibility: 'click', keepVisibleOnInput: false })
+const parseDate = (date) => {
+  var mydate = new Date(date);
+  return `${mydate.getFullYear()}-${mydate.getMonth()+1 < 10 ? `0${mydate.getMonth()+1}`: `${mydate.getMonth()+1}`}-${mydate.getDate() < 10 ? `0${mydate.getDate()}`: `${mydate.getDate()}`}`;
+}
+
+const getStyle = (payload) => {
+  if (parseDate(date.value) == payload) return { 'color': 'white' }
+  if (parseDate(today.value) == payload) return { 'color': '#FC346A' }
+}
+
 const clickdate = (day) => {
   date.value = day
 }
-const onDayClick = (date) => {
-  attributes.value[1].dates = date.id
-  attributes.value[1].highlight.style.background = 'red'
-}
+
+watch(date, (newVal) => {
+  datepicker.value.hidePopover()
+})
+
 onMounted(() => {
-  console.log(datepicker.value)
+  // console.log(datepicker.value)
 })
 </script>
 
 <style>
-
+.vc-popover-caret {
+  display: none;
+}
+.in-next-month.is-not-in-month {
+  display: none;
+}
+.vc-highlight {
+  background-color: #FC346A !important;
+}
+/* .vc-day-content:focus {
+  color: 'white';
+  font-weight: normal;
+  background-color: #FC346A;
+}  */
 </style>
--- src/pages/draggable.vue
+++ src/pages/draggable.vue
@@ -2,13 +2,16 @@
   <draggable 
     v-model="list" 
     group="people" 
-    tag="transition-group" :component-data="{name:'fade', tag:'ul'}"
+    tag="transition-group" :component-data="{name:'fade', tag:'table'}"
     @start="start" 
     @end="end"
-    :move="onMoveCallback"
     item-key="id">
       <template #item="{element}">
-        <li>{{element.name}}</li>
+        <tr>
+          <td>{{ element.id }}</td>
+          <td>{{ element.name }}</td>
+          <td>{{ element.position }}</td>
+        </tr>
       </template>
   </draggable>
 </template>
@@ -23,9 +26,24 @@ const list = ref([
   { id: 3, position: 3, name: 'Dog' }
 ])
 const start = (e) => {
+  console.log(e.target)
+  console.log('I am dragging')
   drag.value = true
 }
 const end = () => {
+  console.log('I am ended dragging')
   drag.value = false
 }
 </script>
+<style scoped>
+tr {
+  padding: 20px;
+}
+td {
+  border: 1px solid grey;
+  padding: 20px;
+}
+table {
+  border: 1px solid grey;
+}
+</style>
